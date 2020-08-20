8b55e137c3cff415765e1797e949ac80b7586373
gulceselim <selimgulce@hotmail.com>
2020-08-20 16:12:04 +0300

    ViewModels files added

---
+++ FoodOrderingManagementSystem/FoodOrderingManagementSystem/ViewModels/OrderVM.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FoodOrderingManagementSystem.ViewModels
+{
+    public partial class OrderVM
+    {
+        [Key]
+        public int order_id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string order_type { get; set; }
+
+        [Required]
+        public string order_details { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string price { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string product_name { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string shipper_name{ get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string user_name { get; set; }
+
+    }
+}
---
+++ FoodOrderingManagementSystem/FoodOrderingManagementSystem/ViewModels/RestaurantVM.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FoodOrderingManagementSystem.ViewModels
+{
+    public partial class RestaurantVM
+    {
+        [Key]
+        public int restaurant_id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string restaurant_name { get; set; }
+
+        [Required]
+        public string restaurant_address { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string username { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string phone_number { get; set; }
+
+        public int? rating { get; set; }
+
+        public int role_id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string city_name { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string city_zip_code { get; set; }
+    }
+}
