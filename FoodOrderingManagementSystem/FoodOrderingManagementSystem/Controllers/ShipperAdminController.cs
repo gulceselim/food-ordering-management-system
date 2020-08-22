beb419d24bc66abe8b0de940b08cdbd5bb1ab23e
gulceselim <selimgulce@hotmail.com>
2020-08-22 19:48:11 +0300

    Update and delete functions have been written. Add function will be written for shipper file

--- FoodOrderingManagementSystem/FoodOrderingManagementSystem/Controllers/ShipperAdminController.cs
+++ FoodOrderingManagementSystem/FoodOrderingManagementSystem/Controllers/ShipperAdminController.cs
@@ -18,5 +18,45 @@ namespace FoodOrderingManagementSystem.Controllers
             ViewBag.restaurants = models.restaurants.ToList();
             return View(shippers);
         }
+        public ActionResult ShipperUpdate(int id)
+        {
+            shipper shipper = models.shippers.FirstOrDefault(x => x.shipper_id == id);
+            return View(shipper);
+        }
+
+        [HttpPost]
+
+        public ActionResult ShipperUpdate(shipper s)
+        {
+            shipper shipper = models.shippers.FirstOrDefault(x => x.shipper_id == s.shipper_id);
+            shipper.first_name = s.first_name;
+            shipper.last_name = s.last_name;
+            shipper.identification_number = s.identification_number;
+            models.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult ShipperAdd()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult ShipperAdd(shipper s)
+        {
+            models.shippers.Add(s);
+            models.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public ActionResult ShipperDelete(int id)
+        {
+            shipper shipper = models.shippers.FirstOrDefault(x => x.shipper_id == id);
+
+            models.shippers.Remove(shipper);
+            models.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
     }
 }
