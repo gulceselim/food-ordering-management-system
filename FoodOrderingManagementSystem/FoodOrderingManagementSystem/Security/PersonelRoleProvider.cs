436b407e3f4c1c94b48b9204a82e01b27d45c2a9
gulceselim <selimgulce@hotmail.com>
2020-08-26 22:57:22 +0300

    Security Folder have been added.

---
+++ FoodOrderingManagementSystem/FoodOrderingManagementSystem/Security/PersonelRoleProvider.cs
@@ -0,0 +1,79 @@
+﻿using FoodOrderingManagementSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+
+namespace FoodOrderingManagementSystem.Security
+{
+    public class PersonelRoleProvider : RoleProvider
+    {
+        public override string ApplicationName
+        {
+            get { throw new NotImplementedException(); }
+            set { throw new NotImplementedException(); }
+        }
+
+        public override void AddUsersToRoles(string[] usernames, string[] roleNames)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void CreateRole(string roleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool DeleteRole(string roleName, bool throwOnPopulatedRole)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string[] FindUsersInRole(string roleName, string usernameToMatch)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string[] GetAllRoles()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string[] GetRolesForUser(string username)
+        {
+            FoodOrderingMSModel models = new FoodOrderingMSModel();
+
+            var user = models.users.FirstOrDefault(x => x.username == username);
+            var restaurant = models.restaurants.FirstOrDefault(x => x.username == username);
+            if(user != null)
+            {
+                return new string[] { user.role.role_name};
+            }else
+            {
+                return new string[] { restaurant.role.role_name };
+            }
+
+        }
+
+        public override string[] GetUsersInRole(string roleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool IsUserInRole(string username, string roleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void RemoveUsersFromRoles(string[] usernames, string[] roleNames)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool RoleExists(string roleName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
